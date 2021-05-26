c90aee6c3f6dacb1ba68973faba1f0239a785237
Parth Sikka <parthsikkah704@gmail.com>
2021-05-26 17:25:10 +0530

    AddListNode Functionality Added

---
+++ AddListNode.js
@@ -0,0 +1,15 @@
+let input = document.querySelector(".todo-input");
+let list = document.querySelector(".todolist");
+input.addEventListener("keypress", function(e){
+    if(e.key == "Enter"){
+        if(input.value!=""){
+        let todo = input.value ; 
+        let li = document.createElement("li") ;
+        li.innerHTML = todo ; 
+        list.append(li) ;
+        todo.value = "" ;
+        }else{
+            alert("Enter a todo!!") ;
+        }
+    }
+})
