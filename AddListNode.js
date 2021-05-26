a1a72d7f1babb85392c164ec80514e9a18474d21
Parth Sikka <parthsikkah704@gmail.com>
2021-05-26 18:08:42 +0530

    All Functions Added

--- AddListNode.js
+++ AddListNode.js
@@ -1,15 +1,22 @@
 let input = document.querySelector(".todo-input");
 let list = document.querySelector(".todolist");
+
 input.addEventListener("keypress", function(e){
     if(e.key == "Enter"){
         if(input.value!=""){
         let todo = input.value ; 
         let li = document.createElement("li") ;
+        li.classList.add("listItem") ;
         li.innerHTML = todo ; 
         list.append(li) ;
+        li.addEventListener("dblclick", handleDeleteNode) ;
         input.value = "" ;
         }else{
             alert("Enter a todo!!") ;
         }
     }
 })
+
+function handleDeleteNode(event){
+    event.target.remove() ;
+}
--- DeleteItem.js
+++
@@ -1,5 +0,0 @@
-let item = document.querySelector(".todolist")
-
-item.addEventListener("dblclick", function(e){
-    e.target.remove() ;
-})
